4d9d64d6565b99b3affb3899211168e41b7afc3e
SUMER <sumersovitkar@outlook.com>
2024-10-13 23:50:38 +0900

    App Servic Plan v4

--- App_Service_Plan/main.tf
+++ App_Service_Plan/main.tf
@@ -12,12 +12,12 @@ resource "azurerm_app_service_plan" "asp" {
     size = var.sku_name
   }
 
-  app_service_environment_id      = lookup(var.optional_values, "app_service_environment_id", null)
-  maximum_elastic_worker_count    = lookup(var.optional_values, "maximum_elastic_worker_count", null)
-  worker_count                    = lookup(var.optional_values, "worker_count", null)
-  per_site_scaling_enabled        = lookup(var.optional_values, "per_site_scaling_enabled", false)
-  zone_balancing_enabled          = lookup(var.optional_values, "zone_balancing_enabled", null)
-  tags                            = lookup(var.optional_values, "tags", {})
+  app_service_environment_id      = lookup(var.app_service_environment_id, "app_service_environment_id", null)
+  maximum_elastic_worker_count    = lookup(var.maximum_elastic_worker_count, "maximum_elastic_worker_count", null)
+  worker_count                    = lookup(var.worker_count, "worker_count", null)
+  per_site_scaling_enabled        = lookup(var.per_site_scaling_enabled, "per_site_scaling_enabled", false)
+  zone_balancing_enabled          = lookup(var.zone_balancing_enabled, "zone_balancing_enabled", null)
+  tags                            = lookup(var.tags, "tags", {})
 
   lifecycle {
     ignore_changes = [per_site_scaling_enabled]
