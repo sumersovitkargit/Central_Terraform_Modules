0d9da4f19ef29b865cd208b50211c5ea77c949b8
SUMER <sumersovitkar@outlook.com>
2024-10-13 22:05:00 +0530

    App Servic Plan v6

--- App_Service_Plan/main.tf
+++ App_Service_Plan/main.tf
@@ -3,6 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_app_service_plan" "asp" {
+  depends_on = [azurerm_resource_group.rg]
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
