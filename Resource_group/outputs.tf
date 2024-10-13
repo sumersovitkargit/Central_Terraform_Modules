649ae196d9dc019e1f162caa9bb7e268a640c7cc
SUMER SOVITKAR <130219318+sumersovitkargit@users.noreply.github.com>
2024-10-13 22:15:57 +0900

    Update outputs.tf

--- Resource_group/outputs.tf
+++ Resource_group/outputs.tf
@@ -9,3 +9,8 @@ output "resource_group_id" {
   description = "The ID of the created resource group"
   value       = azurerm_resource_group.rg.id
 }
+
+output "resource_group_id" {
+  description = "The ID of the created resource group"
+  value       = azurerm_resource_group.rg.name
+}
