26ebff7fecc94b229a5457b0d0ce5b1feab7d3f0
SUMER <sumersovitkar@outlook.com>
2024-10-19 11:35:09 +0530

    App Servic Plan v8

--- App_Service_Windows/main.tf
+++ App_Service_Windows/main.tf
@@ -12,26 +12,26 @@ resource "azurerm_windows_web_app" "webapp" {
     # Add specific site configuration options here
   }
 
-  app_settings                             = lookup(var.app_settings, "app_settings", null)
-  auth_settings                            = lookup(var.auth_settings, "auth_settings", null)
-  auth_settings_v2                         = lookup(var.auth_settings_v2, "auth_settings_v2", null)
-  backup                                   = lookup(var.backup, "backup", null)
-  client_affinity_enabled                  = lookup(var.client_affinity_enabled, "client_affinity_enabled", null)
-  client_certificate_enabled               = lookup(var.client_certificate_enabled, "client_certificate_enabled", null)
-  client_certificate_mode                  = lookup(var.client_certificate_mode, "client_certificate_mode", null)
-  client_certificate_exclusion_paths       = lookup(var.client_certificate_exclusion_paths, "client_certificate_exclusion_paths", null)
-  connection_string                        = lookup(var.connection_string, "connection_string", null)
-  enabled                                  = lookup(var.enabled, "enabled", true)
-  ftp_publish_basic_authentication_enabled = lookup(var.ftp_publish_basic_authentication_enabled, "ftp_publish_basic_authentication_enabled", true)
-  https_only                               = lookup(var.https_only, "https_only", false)
-  public_network_access_enabled            = lookup(var.public_network_access_enabled, "public_network_access_enabled", true)
-  identity                                 = lookup(var.identity, "identity", null)
-  key_vault_reference_identity_id          = lookup(var.key_vault_reference_identity_id, "key_vault_reference_identity_id", null)
-  logs                                     = lookup(var.logs, "logs", null)
-  sticky_settings                          = lookup(var.sticky_settings, "sticky_settings", null)
-  storage_account                          = lookup(var.storage_account, "storage_account", null)
-  tags                                     = lookup(var.tags, "tags", {})
-  virtual_network_subnet_id                = lookup(var.virtual_network_subnet_id, "virtual_network_subnet_id", null)
-  webdeploy_publish_basic_authentication_enabled = lookup(var.webdeploy_publish_basic_authentication_enabled, "webdeploy_publish_basic_authentication_enabled", true)
-  zip_deploy_file                          = lookup(var.zip_deploy_file, "zip_deploy_file", null)
+  app_settings                             = var.app_settings
+  auth_settings                            = var.auth_settings
+  auth_settings_v2                         = var.auth_settings_v2
+  backup                                   = var.backup
+  client_affinity_enabled                  = var.client_affinity_enabled
+  client_certificate_enabled               = var.client_certificate_enabled
+  client_certificate_mode                  = var.client_certificate_mode
+  client_certificate_exclusion_paths       = var.client_certificate_exclusion_paths
+  connection_string                        = var.connection_string
+  enabled                                  = var.enabled
+  ftp_publish_basic_authentication_enabled = var.ftp_publish_basic_authentication_enabled
+  https_only                               = var.https_only
+  public_network_access_enabled            = var.public_network_access_enabled
+  identity                                 = var.identity
+  key_vault_reference_identity_id          = var.key_vault_reference_identity_id
+  logs                                     = var.logs
+  sticky_settings                          = var.sticky_settings
+  storage_account                          = var.storage_account
+  tags                                     = var.tags
+  virtual_network_subnet_id                = var.virtual_network_subnet_id
+  webdeploy_publish_basic_authentication_enabled = var.webdeploy_publish_basic_authentication_enabled
+  zip_deploy_file                          = var.zip_deploy_file
 }
